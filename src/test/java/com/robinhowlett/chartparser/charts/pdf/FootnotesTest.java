24eb776d9d0a4d07d707a9c638422e2641f7721d
Robin Howlett <rhowlett@snaplogic.com>
2018-05-16 15:53:13 -0600

    Attempt at the OS-agnostic version of a test that failed on Windows

--- src/test/java/com/robinhowlett/chartparser/charts/pdf/FootnotesTest.java
+++ src/test/java/com/robinhowlett/chartparser/charts/pdf/FootnotesTest.java
@@ -4,19 +4,13 @@ import com.robinhowlett.chartparser.TestChartResources;
 
 import org.junit.Test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class FootnotesTest {
-
-    private static final String EXPECTED =
-            "PRATER SIXTY FOUR opened a big lead in he stretch and held on for the win. CANDY " +
-                    "SWEETHEART closed well midstretch for place. MIDNIGHTWITHDRAWAL was well " +
-                    "back early and finished with interest. PROWERS COUNTY off slow closed well " +
-                    "mid track. AL BAZ (GB) was up close arly then shuffled back on the inside " +
-                    "and then tired late. COOK INLET raced midpack and had no response in the " +
-                    "stretch. ANTARES DREAM up clsoe early faded after three-quarters. TRAVELIN " +
-                    "TREV was always well back and not a factor.";
 
     private TestChartResources sampleCharts = new TestChartResources();
 
@@ -24,6 +18,18 @@ public class FootnotesTest {
     public void parseFootnotes_WithSampleChartText_ExtractsFootnotesCorrectly() throws Exception {
         String footnotes = Footnotes.parse(sampleCharts.getSampleChartLines(8));
 
-        assertThat(footnotes, equalTo(EXPECTED));
+        StringWriter sw = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(sw);
+        printWriter.append("PRATER SIXTY FOUR opened a big lead in he stretch and held on for the");
+        printWriter.append(" win. CANDY SWEETHEART closed well midstretch for place. ");
+        printWriter.append("MIDNIGHTWITHDRAWAL was well back early and finished with interest. ");
+        printWriter.append("PROWERS COUNTY off slow closed well mid track. AL BAZ (GB) was up ");
+        printWriter.append("close arly then shuffled back on the inside and then tired late. ");
+        printWriter.append("COOK INLET raced midpack and had no response in the ");
+        printWriter.append("stretch. ANTARES DREAM up clsoe early faded after three-quarters. ");
+        printWriter.append("TRAVELIN TREV was always well back and not a factor.");
+        String expected = sw.toString();
+
+        assertThat(footnotes, equalTo(expected));
     }
 }
