7d0fa290a485639ac9fb12654780d54b5d765e8a
Robin Howlett <rhowlett@snaplogic.com>
2018-06-01 15:19:42 -0600

    1.2.0.RELEASE

--- .travis.before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_6c0a552ded21_key -iv $encrypted_6c0a552ded21_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import codesigning.asc
-fi
